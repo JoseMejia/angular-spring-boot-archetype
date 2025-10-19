21943484303157dcfbc3a3b3ba46f61ccc3aee3d
jose <jmejia23@gmail.com>
2025-10-19 18:02:22 +0200

    Added EMPTY_PAGE constant

--- frontend/src/app/models/models.ts
+++ frontend/src/app/models/models.ts
@@ -9,3 +9,16 @@ export interface Page<T> {
   last: boolean;
   empty: boolean;
 }
+
+export const EMPTY_PAGE: Page<never> =
+  {
+    content: [],
+    totalElements: 0,
+    totalPages: 0,
+    size: 10,
+    number: 0,
+    numberOfElements: 0,
+    first: true,
+    last: true,
+    empty: true
+  };
